64f2b0c62d77e5c3a6ed1a368d0ce4301edc032e
Vikram Bageja <vikram.bageja@gmail.com>
2019-10-24 18:33:54 +0100

    initial test setup

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,5 @@
+const env = process.env.NODE_ENV || 'development'
+
 const port = 4000
 const dbURI = 'mongodb://localhost/group-project'
 const secret = 'I am a yogabunny said vikram'
---
+++ test/events/index_spec.js
@@ -0,0 +1,9 @@
+const Event = require('../../models/Event')
+const User = require('../../models/User')
+
+describe('GET /events', () => {
+  it('should work', done => {
+    expect(5).to.eq(5)
+    done()
+  })
+})
