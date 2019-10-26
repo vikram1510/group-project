4830b065ee38365752122f5081794659d3f822b9
Reema Patel <reemapatel17@outlook.com>
2019-10-26 11:50:18 +0100

    moved nav into a separate partial

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -3,7 +3,7 @@ import React from 'react'
 class Home extends React.Component {
   render() {
     return (
-      <div className="home-layout">
+      <div className="home-page">
         <h2>Home Page</h2>
         <div className="tech-lang-wrapper">
           <div></div>
