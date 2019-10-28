33255cc0e404124506d16aec2ea27c7e5905a2cd
Reema Patel <reemapatel17@outlook.com>
2019-10-28 14:51:38 +0000

    half of profile page setup completed

--- src/components/common/Profile.js
+++ src/components/common/Profile.js
@@ -23,16 +23,37 @@ class Profile extends React.Component {
     const { user } = this.state
     if (!user) return null
     return (
-      <div>
-        <h1>Profile</h1>
-        <h4>Username: {user.username}</h4>
-        <h4>Email: {user.email}</h4>
-        <h4>Events Attended</h4>
-        <ul>
-          {user.eventsAttend.map((event, i) =>(
-            <li key={i}>{event.name}</li>
-          ))}
-        </ul>
+      <div className="profile-page">
+        <div className="mini-nav-wrapper">
+          <div className="mini-nav">
+            <h5>Dashboard</h5>
+            <h5>Upcoming Events</h5>
+            <h5>Past Events</h5>
+          </div>
+        </div>
+
+        <div className="dashboard-wrapper">
+          <h1>Profile</h1>
+
+          <div className="dashboard-content">
+            <div className="events-attending-wrapper">
+              <h4>Events Attending</h4>
+              <ul>
+                {user.eventsAttend.map((event, i) =>(
+                  <li key={i}>{event.name}</li>
+                ))}
+              </ul>
+            </div>
+            
+            <div className="your-info-wrapper">
+              <h3>Your Info</h3>
+              <h4>Username: {user.username}</h4>
+              <h4>Email: {user.email}</h4>
+            </div>
+
+          </div>
+
+        </div>
 
       </div>
       
