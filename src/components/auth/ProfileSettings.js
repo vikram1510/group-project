95bfbb7bc597d0847cbd90dde697135fbdab62f9
tom-good <tom.good1990@gmail.com>
2019-10-31 21:12:53 +0000

    all form pages formatted

--- src/components/auth/ProfileSettings.js
+++ src/components/auth/ProfileSettings.js
@@ -41,140 +41,146 @@ class ProfileSettings extends React.Component {
   render() {
     return (
 
-      <div className="settings-page">
+      <div className="auth-page">
+        <div className="form-box-setting">
 
-        <form onSubmit={this.handleSubmit}>
-          <h1>Edit Profile Settings</h1>
+          <form onSubmit={this.handleSubmit}>
+            <div className="form-title-wrapper">
+              <h1>Edit Profile Settings</h1>
+            </div>
 
-          <div className="field">
-            <label className="label">Email:</label>
-            <div className="control">
+            <div className="form-input-wrapper">
+              <label>Email:</label>
               <input
+                className="form-input"
                 name="email"
                 type="text"
                 onChange={this.handleChange}
               />
             </div>
-          </div>
 
-          <div className="field">
-            <label className="label">Password:</label>
-            <div className="control">
+            <div className="form-input-wrapper">
+              <label>Password:</label>
               <input
+                className="form-input"
                 name="email"
                 type="text"
                 onChange={this.handleChange}
               />
             </div>
-          </div>
-
-
-          <div className="category-container">
-            <p className="indent">Preferred language: </p>
-            {/* ROW 1 */}
-            <div className="row1">
-
-              <div className="category-box">
-                <div className="box">
-                  {/* WHITE LOGO */}
-                  <img className="box-initial-image" src="../assets/java-white-button.png"></img>
-                  <div className="box-overlay">
-                    {/* TO COLOUR LOGO */}
-                    <img className="box-after-image" src="../assets/javaT.png"></img>
-                  </div>
-                </div>
-              </div>
-
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/swift-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/swift-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
-
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/js-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/js-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
-
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/php-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/php-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
-
+            <div className="form-input-wrapper">
+              <label>Preferred language: </label>
             </div>
 
-            {/* ROW 2 */}
-            <div className="row2">
-              <p className="category-name">Java</p>
-              <p className="category-name">Swift</p>
-              <p className="category-name">JavaScript</p>
-              <p className="category-name">PHP</p>
+            <div className="category-container-wrapper">
+              <div className="category-container">
+                {/* ROW 1 */}
+                <div className="row1">
+
+                  <div className="category-box">
+                    <div className="box">
+                      {/* WHITE LOGO */}
+                      <img className="box-initial-image" src="../assets/java-white-button.png"></img>
+                      <div className="box-overlay">
+                        {/* TO COLOUR LOGO */}
+                        <img className="box-after-image" src="../assets/javaT.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/swift-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/swift-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/js-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/js-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/php-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/php-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+                {/* ROW 2 */}
+                <div className="row2">
+                  <p className="category-name">Java</p>
+                  <p className="category-name">Swift</p>
+                  <p className="category-name">JavaScript</p>
+                  <p className="category-name">PHP</p>
+                </div>
+
+                {/* ROW 3 */}
+                <div className="row3">
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/c-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/c-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/sql-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/sql-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/python-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/python-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="category-box">
+                    <div className="box">
+                      <img className="box-initial-image" src="../assets/ruby-white-button.png"></img>
+                      <div className="box-overlay">
+                        <img className="box-after-image" src="../assets/ruby-colour-button.png"></img>
+                      </div>
+                    </div>
+                  </div>
+
+                </div>
+
+                {/* ROW 4 */}
+                <div className="row4">
+                  <p className="category-name">C++</p>
+                  <p className="category-name">SQL</p>
+                  <p className="category-name">Python</p>
+                  <p className="category-name">Ruby</p>
+                </div>
+
+              </div>
             </div>
 
-            {/* ROW 3 */}
-            <div className="row3">
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/c-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/c-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
 
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/sql-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/sql-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
+            <button type="submit">Save</button>
+          </form>
+        </div>
 
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/python-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/python-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
-
-              <div className="category-box">
-                <div className="box">
-                  <img className="box-initial-image" src="../assets/ruby-white-button.png"></img>
-                  <div className="box-overlay">
-                    <img className="box-after-image" src="../assets/ruby-colour-button.png"></img>
-                  </div>
-                </div>
-              </div>
-
-            </div>
-
-            {/* ROW 4 */}
-            <div className="row4">
-              <p className="category-name">C++</p>
-              <p className="category-name">SQL</p>
-              <p className="category-name">Python</p>
-              <p className="category-name">Ruby</p>
-            </div>
-
-          </div>
-
-
-          <button type="submit">Save</button>
-        </form>
 
       </div>
 
